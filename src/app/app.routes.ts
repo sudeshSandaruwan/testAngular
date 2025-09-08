6c7faa2af49d782c3f5d65009656304f14e0986e
sudeshSandruwan <62739217+sudeshSandaruwan@users.noreply.github.com>
2025-09-08 16:07:19 +0530

    tes jscript

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
 import { Userview } from './pages/userview/userview';
+import { MainLayout } from './layout/main-layout/main-layout';
 
 export const routes: Routes = [
       {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login',component:LoginPage },
-    {path: 'user',component:Userview}
+    
+    {path: 'main',component:MainLayout,
+      children :[
+      {path: 'user',component:Userview} 
+    ]
+    }
 ];
--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Aside } from '../aside/aside';
 import { Container } from '../container/container';
 import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [Aside,Container,Header],
+  imports: [Aside,Container,Header,Footer],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
